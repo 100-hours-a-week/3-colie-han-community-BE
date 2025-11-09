ba84e35293d45495d8e3bbdf73d4caf06a6a327b
EunSeo0117 <gmjea023@gmail.com>
2025-11-09 21:14:23 +0900

    fix : cors 설정 수정, 파일저장경로수정

---
+++ src/main/java/ktb/week4/config/CorsMvcConfig.java
@@ -0,0 +1,23 @@
+package ktb.week4.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:" + uploadPath + "/");
+    }
+
+}
