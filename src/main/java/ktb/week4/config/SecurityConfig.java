7ef216bdcd0f193f1f8c3a4f566b4f2194a4d3fc
EunSeo0117 <gmjea023@gmail.com>
2025-11-18 15:38:43 +0900

    feat : actuator 경로 오픈

--- src/main/java/ktb/week4/config/SecurityConfig.java
+++ src/main/java/ktb/week4/config/SecurityConfig.java
@@ -88,6 +88,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/signup", "/users/**", "/files/**", "/legal/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
 
